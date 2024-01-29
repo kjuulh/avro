2a72ad5850f4a5806ecc582675fbab51a12f8654
kjuulh <contact@kjuulh.io>
2024-01-29 10:14:07 +0100

    feat: trim avroproto
    Signed-off-by: kjuulh <contact@kjuulh.io>

--- cmd/avroproto/main.go
+++ cmd/avroproto/main.go
@@ -8,19 +8,13 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/kjuulh/avro/v2"
 	"github.com/kjuulh/avro/v2/gen"
 )
 
 type config struct {
-	Pkg         string
-	Out         string
-	Tags        string
-	FullName    bool
-	Encoders    bool
-	Initialisms string
+	Out string
 }
 
 func main() {
@@ -29,16 +23,11 @@ func main() {
 
 func realMain(args []string, stdout, stderr io.Writer) int {
 	var cfg config
-	flgs := flag.NewFlagSet("avrogen", flag.ExitOnError)
+	flgs := flag.NewFlagSet("avroproto", flag.ExitOnError)
 	flgs.SetOutput(stderr)
-	flgs.StringVar(&cfg.Pkg, "pkg", "", "The package name of the output file.")
 	flgs.StringVar(&cfg.Out, "o", "", "The output file path to write to instead of stdout.")
-	flgs.StringVar(&cfg.Tags, "tags", "", "The additional field tags <tag-name>:{snake|camel|upper-camel|kebab}>[,...]")
-	flgs.BoolVar(&cfg.FullName, "fullname", false, "Use the full name of the Record schema to create the struct name.")
-	flgs.BoolVar(&cfg.Encoders, "encoders", false, "Generate encoders for the structs.")
-	flgs.StringVar(&cfg.Initialisms, "initialisms", "", "Custom initialisms <VAL>[,...] for struct and field names.")
 	flgs.Usage = func() {
-		_, _ = fmt.Fprintln(stderr, "Usage: avrogen [options] schemas")
+		_, _ = fmt.Fprintln(stderr, "Usage: avroproto [options] schemas")
 		_, _ = fmt.Fprintln(stderr, "Options:")
 		flgs.PrintDefaults()
 	}
@@ -51,24 +40,8 @@ func realMain(args []string, stdout, stderr io.Writer) int {
 		return 1
 	}
 
-	tags, err := parseTags(cfg.Tags)
-	if err != nil {
-		_, _ = fmt.Fprintln(stderr, "Error: "+err.Error())
-		return 1
-	}
-
-	initialisms, err := parseInitialisms(cfg.Initialisms)
-	if err != nil {
-		_, _ = fmt.Fprintln(stderr, "Error: "+err.Error())
-		return 1
-	}
-
-	opts := []gen.OptsFunc{
-		gen.WithFullName(cfg.FullName),
-		gen.WithEncoders(cfg.Encoders),
-		gen.WithInitialisms(initialisms),
-	}
-	g := gen.NewGenerator(cfg.Pkg, tags, opts...)
+	opts := []gen.OptsFunc{}
+	g := gen.NewGenerator("testpkg", map[string]gen.TagStyle{}, opts...)
 	for _, file := range flgs.Args() {
 		schema, err := avro.ParseFiles(filepath.Clean(file))
 		if err != nil {
@@ -79,7 +52,7 @@ func realMain(args []string, stdout, stderr io.Writer) int {
 	}
 
 	var buf bytes.Buffer
-	if err = g.Write(&buf); err != nil {
+	if err := g.Write(&buf); err != nil {
 		_, _ = fmt.Fprintf(stderr, "Error: could not generate code: %v\n", err)
 		return 3
 	}
@@ -114,60 +87,5 @@ func validateOpts(nargs int, cfg config) error {
 		return fmt.Errorf("at least one schema is required")
 	}
 
-	if cfg.Pkg == "" {
-		return fmt.Errorf("a package is required")
-	}
-
 	return nil
-}
-
-func parseTags(raw string) (map[string]gen.TagStyle, error) {
-	if raw == "" {
-		return map[string]gen.TagStyle{}, nil
-	}
-
-	result := map[string]gen.TagStyle{}
-	for _, tag := range strings.Split(raw, ",") {
-		parts := strings.Split(tag, ":")
-		switch {
-		case len(parts) != 2:
-			return nil, fmt.Errorf("%q is not a valid tag, should be in the formet \"tag:style\"", tag)
-		case parts[0] == "":
-			return nil, fmt.Errorf("tag name is required in %q", tag)
-		}
-
-		var style gen.TagStyle
-		switch strings.ToLower(parts[1]) {
-		case string(gen.UpperCamel):
-			style = gen.UpperCamel
-		case string(gen.Camel):
-			style = gen.Camel
-		case string(gen.Kebab):
-			style = gen.Kebab
-		case string(gen.Snake):
-			style = gen.Snake
-		case string(gen.Original):
-			style = gen.Original
-		default:
-			return nil, fmt.Errorf("style %q is invalid in %q", parts[1], tag)
-		}
-		result[parts[0]] = style
-	}
-	return result, nil
-}
-
-func parseInitialisms(raw string) ([]string, error) {
-	if raw == "" {
-		return []string{}, nil
-	}
-
-	result := []string{}
-	for _, initialism := range strings.Split(raw, ",") {
-		if initialism != strings.ToUpper(initialism) {
-			return nil, fmt.Errorf("initialism %q must be fully in upper case", initialism)
-		}
-		result = append(result, initialism)
-	}
-
-	return result, nil
 }
--- cmd/avroproto/main_test.go
+++ cmd/avroproto/main_test.go
@@ -14,43 +14,12 @@ import (
 
 var update = flag.Bool("update", false, "Update golden files")
 
-func TestAvroGen_RequiredFlags(t *testing.T) {
+func TestAvroProto_RequiredFlags(t *testing.T) {
 	tests := []struct {
 		name         string
 		args         []string
 		wantExitCode int
-	}{
-		{
-			name:         "validates schema is set",
-			args:         []string{"avrogen", "-pkg", "test", "-o", "some/file"},
-			wantExitCode: 1,
-		},
-		{
-			name:         "validates schema exists",
-			args:         []string{"avrogen", "-pkg", "test", "-o", "some/file", "some/schema"},
-			wantExitCode: 2,
-		},
-		{
-			name:         "validates package is set",
-			args:         []string{"avrogen", "-o", "some/file", "schema.avsc"},
-			wantExitCode: 1,
-		},
-		{
-			name:         "validates tag format are valid",
-			args:         []string{"avrogen", "-o", "some/file", "-pkg", "test", "-tags", "snake", "schema.avsc"},
-			wantExitCode: 1,
-		},
-		{
-			name:         "validates tag key are valid",
-			args:         []string{"avrogen", "-o", "some/file", "-pkg", "test", "-tags", ":snake", "schema.avsc"},
-			wantExitCode: 1,
-		},
-		{
-			name:         "validates tag style are valid",
-			args:         []string{"avrogen", "-o", "some/file", "-pkg", "test", "-tags", "json:something", "schema.avsc"},
-			wantExitCode: 1,
-		},
-	}
+	}{}
 
 	for _, test := range tests {
 		test := test
@@ -62,10 +31,10 @@ func TestAvroGen_RequiredFlags(t *testing.T) {
 	}
 }
 
-func TestAvroGen_GeneratesSchemaStdout(t *testing.T) {
+func TestAvroProto_GeneratesSchemaStdout(t *testing.T) {
 	var buf bytes.Buffer
 
-	args := []string{"avrogen", "-pkg", "testpkg", "testdata/schema.avsc"}
+	args := []string{"avroproto", "testdata/schema.avsc"}
 	gotCode := realMain(args, &buf, io.Discard)
 	require.Equal(t, 0, gotCode)
 
@@ -74,13 +43,13 @@ func TestAvroGen_GeneratesSchemaStdout(t *testing.T) {
 	assert.Equal(t, want, buf.Bytes())
 }
 
-func TestAvroGen_GeneratesSchema(t *testing.T) {
-	path, err := os.MkdirTemp("./", "avrogen")
+func TestAvroProto_GeneratesSchema(t *testing.T) {
+	path, err := os.MkdirTemp("./", "avroproto")
 	require.NoError(t, err)
 	t.Cleanup(func() { _ = os.RemoveAll(path) })
 
 	file := filepath.Join(path, "test.go")
-	args := []string{"avrogen", "-pkg", "testpkg", "-o", file, "testdata/schema.avsc"}
+	args := []string{"avroproto", "-o", file, "testdata/schema.avsc"}
 	gotCode := realMain(args, io.Discard, io.Discard)
 	require.Equal(t, 0, gotCode)
 
@@ -95,120 +64,4 @@ func TestAvroGen_GeneratesSchema(t *testing.T) {
 	want, err := os.ReadFile("testdata/golden.go")
 	require.NoError(t, err)
 	assert.Equal(t, want, got)
-}
-
-func TestAvroGen_GeneratesSchemaWithFullname(t *testing.T) {
-	path, err := os.MkdirTemp("./", "avrogen")
-	require.NoError(t, err)
-	t.Cleanup(func() { _ = os.RemoveAll(path) })
-
-	file := filepath.Join(path, "test.go")
-	args := []string{"avrogen", "-pkg", "testpkg", "-o", file, "-fullname", "testdata/schema.avsc"}
-	gotCode := realMain(args, io.Discard, io.Discard)
-	require.Equal(t, 0, gotCode)
-
-	got, err := os.ReadFile(file)
-	require.NoError(t, err)
-
-	if *update {
-		err = os.WriteFile("testdata/golden_fullname.go", got, 0600)
-		require.NoError(t, err)
-	}
-
-	want, err := os.ReadFile("testdata/golden_fullname.go")
-	require.NoError(t, err)
-	assert.Equal(t, want, got)
-}
-
-func TestAvroGen_GeneratesSchemaWithEncoders(t *testing.T) {
-	path, err := os.MkdirTemp("./", "avrogen")
-	require.NoError(t, err)
-	t.Cleanup(func() { _ = os.RemoveAll(path) })
-
-	file := filepath.Join(path, "test.go")
-	args := []string{"avrogen", "-pkg", "testpkg", "-o", file, "-encoders", "testdata/schema.avsc"}
-	gotCode := realMain(args, io.Discard, io.Discard)
-	require.Equal(t, 0, gotCode)
-
-	got, err := os.ReadFile(file)
-	require.NoError(t, err)
-
-	if *update {
-		err = os.WriteFile("testdata/golden_encoders.go", got, 0600)
-		require.NoError(t, err)
-	}
-
-	want, err := os.ReadFile("testdata/golden_encoders.go")
-	require.NoError(t, err)
-	assert.Equal(t, want, got)
-}
-
-func TestParseTags(t *testing.T) {
-	tests := []struct {
-		name string
-		tags string
-	}{
-		{
-			name: "snake case",
-			tags: "json:snake",
-		},
-		{
-			name: "camel case",
-			tags: "json:camel",
-		},
-		{
-			name: "upper camel case",
-			tags: "json:upper-camel",
-		},
-		{
-			name: "kebab case",
-			tags: "json:kebab",
-		},
-		{
-			name: "original case",
-			tags: "json:original",
-		},
-	}
-
-	for _, test := range tests {
-		test := test
-		t.Run(test.name, func(t *testing.T) {
-			_, err := parseTags(test.tags)
-
-			require.NoError(t, err)
-		})
-	}
-}
-
-func TestParseInitialisms(t *testing.T) {
-	tests := []struct {
-		name        string
-		initialisms string
-		errFunc     assert.ErrorAssertionFunc
-	}{
-		{
-			name:        "single initialism",
-			initialisms: "ABC",
-			errFunc:     assert.NoError,
-		},
-		{
-			name:        "multiple initialisms",
-			initialisms: "ABC,DEF",
-			errFunc:     assert.NoError,
-		},
-		{
-			name:        "wrong initialism",
-			initialisms: "ABC,def,GHI",
-			errFunc:     assert.Error,
-		},
-	}
-
-	for _, test := range tests {
-		test := test
-		t.Run(test.name, func(t *testing.T) {
-			_, err := parseInitialisms(test.initialisms)
-
-			test.errFunc(t, err)
-		})
-	}
 }
